9927e5e11b7b65a4758864babe541761bd83d0a6
Khaichi <mustbekamal@gmail.com>
2019-02-13 21:57:31 +0700

    Labpy1

---
+++ Labpy1/Labpy1.py
@@ -0,0 +1,12 @@
+#!/user/bin/python3
+print ('menentukan bilangan terbesar')
+print ('masukan 3 bilangan yang diinginkan')
+a = int (input ('bilangan pertama = '))
+b = int (input ('bilangan kedua = '))
+c = int (input ('bilangan ketiga = '))
+if a>b and a>c :
+    print ('bilangan terbesar =',a)
+elif b>a and b>c :
+    print ('bilangan terbesar =',b)
+else :
+    print (c,'bilangan terbesar =',c)
